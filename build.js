c5d6416bc4987d35adfc3cb1fa8575f77dc6985a
Sam Beller <sammbeller@gmail.com>
2024-04-24 20:24:29 -0400

    build script and index page

--- build.js
+++ build.js
@@ -1,1 +1,17 @@
+import fs from "fs";
 import mustache from "mustache";
+import issue1View from "./views/issues/issue1.js";
+
+if (!fs.existsSync("build")) {
+  fs.mkdirSync("build");
+  fs.cpSync("style.css", "build/style.css");
+}
+
+const indexTemplate = fs.readFileSync("index.mustache", "utf-8");
+const entryBoxPartial = fs.readFileSync("partials/entry-box.mustache", "utf-8");
+
+const indexContent = mustache.render(indexTemplate, issue1View, {
+  "entry-box": entryBoxPartial,
+});
+fs.writeFileSync("build/index.html", indexContent);
+
--- views/issues/issue1.js
+++ views/issues/issue1.js
@@ -1,12 +1,26 @@
-export const view = {
+export default {
   issue: 1,
   entries: [
     {
       contributor: "cs",
-      displayGenre: () => this.genre.slice(0,1).toUpperCase() + this.genre.slice(1),
-      entryURL: () => this.contributor + "/" + this.title + ".html",
+      displayGenre: function() { return this.genre.slice(0,1).toUpperCase() + this.genre.slice(1) },
+      entryURL: function() { return "contributor/" + this.contributor + "/" + this.title + ".html" },
       genre: "poetry",
       title: "untitled"
-    }
+    },
+    {
+      contributor: "roderic",
+      displayGenre: function() { return this.genre.slice(0,1).toUpperCase() + this.genre.slice(1) },
+      entryURL: function() { return "contributor/" + this.contributor + "/" + this.title + ".html" },
+      genre: "poetry",
+      title: "endospore"
+    },
+    {
+      contributor: "sleazy-b",
+      displayGenre: function() { return this.genre.slice(0,1).toUpperCase() + this.genre.slice(1) },
+      entryURL: function() { return "contributor/" + this.contributor + "/" + this.title + ".html" },
+      genre: "poetry",
+      title: "Cherrystone"
+    },
   ]
 };
