0ad91d192cf8230edd3812fde9bb0aa3720e7cc6
Sam Beller <sammbeller@gmail.com>
2024-04-26 10:42:50 -0400

    view for mick story and adjust url code

--- views/issues/issue1.js
+++ views/issues/issue1.js
@@ -7,7 +7,8 @@ export default {
         return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
       },
       entryURL: function () {
-        return "contributor/" + this.contributor + "/" + this.title;
+        const titleForURL = this.title.toLowerCase().split(" ").join("-");
+        return "contributor/" + this.contributor + "/" + titleForURL;
       },
       genre: "poetry",
       title: "untitled",
@@ -18,7 +19,8 @@ export default {
         return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
       },
       entryURL: function () {
-        return "contributor/" + this.contributor + "/" + this.title;
+        const titleForURL = this.title.toLowerCase().split(" ").join("-");
+        return "contributor/" + this.contributor + "/" + titleForURL;
       },
       genre: "poetry",
       title: "endospore",
@@ -29,10 +31,23 @@ export default {
         return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
       },
       entryURL: function () {
-        return "contributor/" + this.contributor + "/" + this.title;
+        const titleForURL = this.title.toLowerCase().split(" ").join("-");
+        return "contributor/" + this.contributor + "/" + titleForURL;
       },
       genre: "poetry",
       title: "Cherrystone",
+    },
+    {
+      contributor: "pinkbubblesgo",
+      displayGenre: function () {
+        return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
+      },
+      entryURL: function () {
+        const titleForURL = this.title.toLowerCase().split(" ").join("-");
+        return "contributor/" + this.contributor + "/" + titleForURL;
+      },
+      genre: "fiction",
+      title: "Mick is My Darling",
     },
   ],
 };
