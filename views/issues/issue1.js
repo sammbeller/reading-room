589dbab8b62e94fe7a30b63fcea5c1ebfa0293ba
Sam Beller <sammbeller@gmail.com>
2024-04-26 11:58:50 -0400

    extract views functions

--- views/issues/issue1.js
+++ views/issues/issue1.js
@@ -1,51 +1,40 @@
+function displayGenre() {
+  return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
+}
+
+function entryURL() {
+  const titleForURL = this.title.toLowerCase().split(" ").join("-");
+  return "contributor/" + this.contributor + "/" + titleForURL;
+}
+
 export default {
   issue: 1,
   entries: [
     {
       contributor: "cs",
-      displayGenre: function () {
-        return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
-      },
-      entryURL: function () {
-        const titleForURL = this.title.toLowerCase().split(" ").join("-");
-        return "contributor/" + this.contributor + "/" + titleForURL;
-      },
+      displayGenre,
+      entryURL,
       genre: "poetry",
       title: "untitled",
     },
     {
       contributor: "roderic",
-      displayGenre: function () {
-        return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
-      },
-      entryURL: function () {
-        const titleForURL = this.title.toLowerCase().split(" ").join("-");
-        return "contributor/" + this.contributor + "/" + titleForURL;
-      },
+      displayGenre,
+      entryURL,
       genre: "poetry",
       title: "endospore",
     },
     {
       contributor: "sleazy-b",
-      displayGenre: function () {
-        return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
-      },
-      entryURL: function () {
-        const titleForURL = this.title.toLowerCase().split(" ").join("-");
-        return "contributor/" + this.contributor + "/" + titleForURL;
-      },
+      displayGenre,
+      entryURL,
       genre: "poetry",
       title: "Cherrystone",
     },
     {
       contributor: "pinkbubblesgo",
-      displayGenre: function () {
-        return this.genre.slice(0, 1).toUpperCase() + this.genre.slice(1);
-      },
-      entryURL: function () {
-        const titleForURL = this.title.toLowerCase().split(" ").join("-");
-        return "contributor/" + this.contributor + "/" + titleForURL;
-      },
+      displayGenre,
+      entryURL,
       genre: "fiction",
       title: "Mick is My Darling",
     },
