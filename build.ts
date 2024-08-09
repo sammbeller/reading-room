06a584642d40f4f83dd49b17b12e68074bf44c21
Sam Beller <sammbeller@gmail.com>
2024-08-09 11:23:52 -0400

    Work on build script, adjusting relative url for distinction, and menu

--- build.ts
+++ build.ts
@@ -9,6 +9,7 @@ if (!fs.existsSync("build")) {
 }
 
 const indexTemplate = fs.readFileSync("index.mustache", "utf-8");
+const menuPartial = fs.readFileSync("partials/menu.mustache", "utf-8");
 const entryBoxPartial = fs.readFileSync("partials/entry-box.mustache", "utf-8");
 const issueDisplayPartial = fs.readFileSync(
   "partials/issue-display.mustache",
@@ -18,52 +19,56 @@ const htmlHeaderPartial = fs.readFileSync(
   "partials/html-header.mustache",
   "utf-8"
 );
-const view = getView();
-const issue1View = view.issues[0].toObject();
 
-const indexContent = mustache.render(indexTemplate, view.toObject(), {
+const view = getView().toObject();
+
+const indexContent = mustache.render(indexTemplate, view, {
   "entry-box": entryBoxPartial,
   "html-header": htmlHeaderPartial,
   "issue-display": issueDisplayPartial,
+  menu: menuPartial,
 });
 fs.writeFileSync("build/index.html", indexContent);
 
 fs.mkdirSync("build/contributor");
 
-issue1View.entries.forEach((entry) => {
-  // Build and write the entry template
-  if (!fs.existsSync(`build/contributor/${entry.contributor}`)) {
-    fs.mkdirSync(`build/contributor/${entry.contributor}`);
-  }
-  const template = fs.readFileSync("templates/entry.mustache", "utf-8");
-  const partial = fs.readFileSync(entry.entryFile, "utf-8");
+view.issues.forEach((issue) => {
+  issue.entries.forEach((entry) => {
+    // Build and write the entry template
+    if (!fs.existsSync(`build/contributor/${entry.contributor}`)) {
+      fs.mkdirSync(`build/contributor/${entry.contributor}`);
+    }
+    const template = fs.readFileSync("templates/entry.mustache", "utf-8");
+    const partial = fs.readFileSync(entry.entryFile, "utf-8");
 
-  const entryContent = mustache.render(template, entry, {
-    entry: partial,
-    "html-header": htmlHeaderPartial,
-  });
-  fs.writeFileSync("build/" + entry.entryURL, entryContent);
-
-  // Find additional content and move it over
-  if (entry.additionalContent && entry.additionalContent.length > 0) {
-    entry.additionalContent.forEach((additionalContent) => {
-      const basePath = `contributor/${entry.contributor}`;
-      const additionalContentPath = path.join(basePath, additionalContent);
-      if (!fs.existsSync(additionalContentPath)) {
-        console.warn(
-          `Could not find additional content for ${entry.entryFile}: ${additionalContent}`
-        );
-      } else {
-        console.log(
-          `Adding additional content for ${entry.entryFile}: ${additionalContent}`
-        );
-        const additionalContentTargetPath = path.join(
-          "build/contributor",
-          entry.contributor,
-          additionalContent
-        );
-        fs.copyFileSync(additionalContentPath, additionalContentTargetPath);
-      }
+    const entryContent = mustache.render(template, entry, {
+      entry: partial,
+      "html-header": htmlHeaderPartial,
+      menu: menuPartial,
     });
-  }
+    fs.writeFileSync("build/" + entry.entryURL, entryContent);
+
+    // Find additional content and move it over
+    if (entry.additionalContent && entry.additionalContent.length > 0) {
+      entry.additionalContent.forEach((additionalContent) => {
+        const basePath = `contributor/${entry.contributor}`;
+        const additionalContentSourcePath = path.join(basePath, additionalContent);
+        if (!fs.existsSync(additionalContentSourcePath)) {
+          console.warn(
+            `Could not find additional content for ${entry.entryFile}: ${additionalContent}`
+          );
+        } else {
+          console.log(
+            `Adding additional content for ${entry.entryFile}: ${additionalContent}`
+          );
+          const additionalContentTargetPath = path.join(
+            "build/contributor",
+            entry.contributor,
+            additionalContent
+          );
+          fs.copyFileSync(additionalContentSourcePath, additionalContentTargetPath);
+        }
+      });
+    }
+  });
 });
